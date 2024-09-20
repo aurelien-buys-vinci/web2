fe9eee915a1a42095f175ce985b81d807e3300a6
buys <aurelien.buys@student.vinci.be>
2024-09-20 10:28:24 +0200

    new: ex1.1

---
+++ exercices/1.1/basic-ts-api-boilerplate/routes/films.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import path from "node:path";
+import { Film } from "../types";
+import { serialize, parse } from "../utils/json";
+const router = Router();
+
+
+
+
+
+
+export default router;
