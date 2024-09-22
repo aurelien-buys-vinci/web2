c61d3c062cfdf66c145e2da769c6132667bf5fa1
buys <aurelien.buys@student.vinci.be>
2024-09-22 12:42:04 +0200

    new: ex1.1

--- exercices/1.1/basic-ts-api-boilerplate/routes/films.ts
+++ exercices/1.1/basic-ts-api-boilerplate/routes/films.ts
@@ -1,12 +1,46 @@
 import { Router } from "express";
-import path from "node:path";
 import { Film } from "../types";
-import { serialize, parse } from "../utils/json";
+
+
 const router = Router();
 
+const films: Film[] = [
+    {
+      id: 1,
+      title: "Inception",
+      director: "Christopher Nolan",
+      duration: 148,
+      budget: 160,
+      description: "A mind-bending thriller about dreams within dreams.",
+      imageUrl: "https://example.com/inception.jpg"
+    },
+    {
+      id: 2,
+      title: "The Matrix",
+      director: "The Wachowskis",
+      duration: 136,
+      description: "A hacker discovers reality as he knows it is a simulation.",
+      imageUrl: "https://example.com/matrix.jpg"
+    },
+    {
+      id: 3,
+      title: "Interstellar",
+      director: "Christopher Nolan",
+      duration: 169,
+      budget: 165
+    },
+    {
+      id: 4,
+      title: "Parasite",
+      director: "Bong Joon Ho",
+      duration: 132,
+      description: "A darkly comedic thriller about class struggles.",
+    }
+  ];
 
-
-
+router.get("/", (req,res) => {
+    return res.json(films);
+});
 
 
 export default router;
