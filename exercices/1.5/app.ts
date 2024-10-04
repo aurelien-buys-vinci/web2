867c0a43d9b4be1f58c567ba47ebed9374560805
buys <aurelien.buys@student.vinci.be>
2024-10-04 09:58:29 +0200

    new: ex1.5

---
+++ exercices/1.5/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+
+import usersRouter from "./routes/users";
+import pizzaRouter from "./routes/pizzas";
+import filmsRouter from "./routes/films";
+
+const app = express();
+let counter=0;
+app.use((req, _res, next) => {
+    if(req.method === "GET"){
+        counter++;
+    }
+    console.log("GET counter : " + counter);
+    next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/users", usersRouter);
+app.use("/pizzas", pizzaRouter);
+app.use("/films", filmsRouter);
+
+
+
+
+export default app;
