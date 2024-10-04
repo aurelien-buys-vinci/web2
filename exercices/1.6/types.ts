e6faef74aa2b9a733e322ff7c1d68b5bc23251d5
buys <aurelien.buys@student.vinci.be>
2024-10-04 10:13:15 +0200

    new: 1.6

---
+++ exercices/1.6/types.ts
@@ -0,0 +1,27 @@
+interface Pizza {
+  id: number;
+  title: string;
+  content: string;
+}
+
+interface PizzaToUpdate {
+  title?: string;
+  content?: string;
+}
+
+type NewPizza = Omit<Pizza, "id">;
+
+interface Film {
+  id: number;
+  title: string;
+  director: string;
+  duration: number;
+  budget?: number;
+  description?: string;
+  imageUrl?: string;
+}
+
+type newFilm= Omit<Film, "id">;
+
+
+export type { Pizza, NewPizza, PizzaToUpdate, Film, newFilm };
