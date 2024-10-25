dec9c3c32335e1b4a625fc728fcdf302d9382827
buys <aurelien.buys@student.vinci.be>
2024-10-25 10:32:25 +0200

    new:ex2.5-events:pas fini

--- exercices/2.5/src/components/Main/index.tsx
+++ exercices/2.5/src/components/Main/index.tsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
 
 interface ClickCounterProps {
-    message: string;
+    message1: string;
 }
 
-const ClickCounter = ({message}:ClickCounterProps) => {
+const ClickCounter = ({message1}:ClickCounterProps) => {
     const [count, setCount] = useState(0)
+    const [mouseOn, setMouseOn] = useState(false)
+
+    const handleMouse = () => {
+        console.log("mouse over")
+        setMouseOn(!mouseOn)
+    }
+    
     
     return (
-        <button onClick={() => setCount((count) => count + 1)}>
-            {count >= 10 ? message : ""}
+        <button onClick={() => setCount((count) => count + 1)} onMouse={handleMouse}>
+            {count >= 10 ? message1 : ""}
             count is {count}
         </button>
     )
