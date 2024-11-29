2ea0c2643b00c0c2fa0fd1b7a08a475000219aa6
buys <aurelien.buys@student.vinci.be>
2024-11-29 09:32:17 +0100

    new:ex2.14b+

--- exercices/2.14b/src/components/RandomDog/index.tsx
+++ exercices/2.14b/src/components/RandomDog/index.tsx
@@ -1,37 +1,49 @@
-import { useEffect, useState } from "react"
-
+import { useEffect, useState } from "react";
 
 const RandomDog = () => {
-    const [dog, setDog] = useState<string>("");
+  const [dog, setDog] = useState<string>("");
+  const [mouseOn, setMouseOn] = useState<boolean>(false);
 
-    useEffect(() => {
+  const handleMouseOn = () => {
+    setMouseOn(!mouseOn);
+  };
+
+  useEffect(() => {
+    if (!mouseOn) {
+      fetchDog();
+      const interval = setInterval(() => {
         fetchDog();
-        const interval = setInterval(() => {
-          fetchDog();
-        }, 5000);
-        return () => clearInterval(interval);
-      }, []);
+      }, 5000);
+      return () => clearInterval(interval);
+    }
+  }, [mouseOn]);
 
-      const fetchDog = async () => {
-        try {
-          const response = await fetch("https://dog.ceo/api/breeds/image/random");
-    
-          if (!response.ok)
-            throw new Error(
-              `fetch error : ${response.status} : ${response.statusText}`
-            );
-    
-          const dog = await response.json();
-          setDog(dog.message);
-        } catch (err) {
-          console.error("HomePage::error: ", err);
-        }
-      }
-    return (
-        <div>
-            <img src={dog} alt="dog" />
-        </div>
-    )
-}
+  const fetchDog = async () => {
+    try {
+      const response = await fetch("https://dog.ceo/api/breeds/image/random");
+
+      if (!response.ok)
+        throw new Error(
+          `fetch error : ${response.status} : ${response.statusText}`
+        );
+
+      const dog = await response.json();
+      setDog(dog.message);
+    } catch (err) {
+      console.error("HomePage::error: ", err);
+    }
+  };
+  return (
+    <div>
+      <img
+        src={dog}
+        alt="dog"
+        height={300}
+        onMouseEnter={handleMouseOn}
+        onMouseLeave={handleMouseOn}
+      />
+    </div>
+  );
+};
 
 export default RandomDog;
